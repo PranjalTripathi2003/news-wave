877c5ff351ab96b4d2a897e145a01d4af1c49747
PranjalTripathi2003 <tripathipranjal1711@gmail.com>
2024-07-27 09:27:02 +0530

    Configure GitHub Actions for deployment

--- .history/vite.config_20240727091407.js
+++ .history/vite.config_20240727091407.js
@@ -2,7 +2,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/NewsWave/", // Replace with your repository name
+  base: "/NewsWave/", // Use a leading and trailing slash
 });
