d38096b89c0f3217a9a2b0b9adc885a6e4c7811c
PranjalTripathi2003 <tripathipranjal1711@gmail.com>
2024-07-27 09:50:23 +0530

    Configure GitHub Actions for deployment

---
+++ .history/src/components/NewsBoard_20240727095003.jsx
@@ -0,0 +1,19 @@
+useEffect(() => {
+  setLoading(true);
+  let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setArticles(data.articles || []); // Fallback to an empty array if articles is undefined
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.error('Fetching error: ', error);
+      setLoading(false);
+    });
+}, [category]);
--- .history/src/components/NewsBoard_20240727095005.jsx
+++ .history/src/components/NewsBoard_20240727095005.jsx
@@ -12,23 +12,13 @@ const NewsBoard = ({ category, darkMode }) => {
     let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${
       import.meta.env.VITE_API_KEY
     }`;
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setArticles(data.articles || []); // Fallback to an empty array if articles is undefined
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Fetching error: ", error);
+    fetch(url).then((response) => {
+      response.json().then((data) => {
+        setArticles(data.articles);
         setLoading(false);
       });
+    });
   }, [category]);
-
 
   return (
     <div className={darkMode ? "dark-mode" : "light-mode"}>
