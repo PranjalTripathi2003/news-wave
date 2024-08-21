57b3ac4fe21fea172b70c874930c664981bcf76c
PranjalTripathi2003 <tripathipranjal1711@gmail.com>
2024-08-21 10:00:09 +0530

    chore: Update Vercel configuration files and add new vercel.json file

--- .history/vite.config_20240821095945.js
+++ .history/vite.config_20240821095945.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/NewsWave/", // Replace with your repository name
   build: {
     outDir: "dist", // This should specify the output directory as 'dist'
   },
